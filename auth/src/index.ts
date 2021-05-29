c1d1e909b22655274f2c2c4bc1f10b079b310efa
codeinsack <codeinsack@gmail.com>
2021-05-29 18:10:19 +0300

    install cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/currentUser';
 import { signInRouter } from './routes/signIn';
@@ -11,7 +12,9 @@ import { errorHandler } from './middlewares/errorHandler';
 import { NotFoundError } from './errors/NotFoundError';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
 
 app.use(currentUserRouter);
 app.use(signInRouter);
