96cb82ef6b5aebcaaaaf17f992663a81929610d4
codeinsack <codeinsack@gmail.com>
2021-05-30 15:33:40 +0300

    add validateRequest middleware

--- auth/src/routes/signIn.ts
+++ auth/src/routes/signIn.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validateRequest';
 
 const router = express.Router();
 
-router.post('/api/users/signIn', (req, res) => {
-  res.send('Sign In Route');
-});
+router.post(
+  '/api/users/signIn',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {},
+);
 
 export { router as signInRouter };
