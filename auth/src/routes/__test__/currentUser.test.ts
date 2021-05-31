4a64692fbd3be0fa481144bb15656875bb324884
codeinsack <codeinsack@gmail.com>
2021-05-31 23:52:42 +0300

    implement global signIn function

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -2,14 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('Responds with details about the current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signUp')
-    .send({
-      email: 'test@test.io',
-      password: 'pass123',
-    })
-    .expect(201);
-  const cookie = authResponse.get('Set-Cookie');
+  const cookie = await global.signIn();
 
   const response = await request(app)
     .get('/api/users/currentUser')
@@ -17,5 +10,5 @@ it('Responds with details about the current user', async () => {
     .send()
     .expect(200);
 
-  expect(response.body.currentUser.email).toEqual('test@test.io');
+  expect(response.body.currentUser.email).toEqual('test@mail.io');
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signIn(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -27,3 +37,18 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signIn = async () => {
+  const email = 'test@mail.io';
+  const password = 'pass123';
+
+  const response = await request(app)
+    .post('/api/users/signUp')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  return response.get('Set-Cookie');
+};
