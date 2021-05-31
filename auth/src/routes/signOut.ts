7512284db62fd999a15c7aa44a5bcadee1e29754
codeinsack <codeinsack@gmail.com>
2021-05-31 20:54:08 +0300

    implement sign out route

--- auth/src/routes/signOut.ts
+++ auth/src/routes/signOut.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signOut', (req, res) => {
-  res.send('Sign Out Route');
+  req.session = null;
+  res.send({});
 });
 
 export { router as signOutRouter };
