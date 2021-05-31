4b5029326aa71182a2a18a38df12f5d5e9199c9e
codeinsack <codeinsack@gmail.com>
2021-05-31 22:33:59 +0300

    add dependencies for tests

---
+++ auth/src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/currentUser';
+import { signInRouter } from './routes/signIn';
+import { signOutRouter } from './routes/signOut';
+import { signUpRouter } from './routes/signUp';
+import { errorHandler } from './middlewares/errorHandler';
+import { NotFoundError } from './errors/NotFoundError';
+import { start } from './index';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({ signed: false, secure: true }));
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+start().then();
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
+import { app } from './app';
 
-import { currentUserRouter } from './routes/currentUser';
-import { signInRouter } from './routes/signIn';
-import { signOutRouter } from './routes/signOut';
-import { signUpRouter } from './routes/signUp';
-import { errorHandler } from './middlewares/errorHandler';
-import { NotFoundError } from './errors/NotFoundError';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(cookieSession({ signed: false, secure: true }));
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
-
-const start = async () => {
+export const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
