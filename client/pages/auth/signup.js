38895478d4e7eed84a4892fda9be20a586034e03
codeinsack <codeinsack@gmail.com>
2021-06-01 19:13:51 +0300

    add handlers for signup form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,36 @@
+import { useState } from "react";
+
 export default () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(email, password);
+    setEmail("");
+    setPassword("");
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input
+          className="form-control"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input
+          className="form-control"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </div>
       <button className="btn btn-primary">Sign Up</button>
     </form>
