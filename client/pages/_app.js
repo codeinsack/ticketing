33468be1784ac614cddb9a441a1b0040963be160
codeinsack <codeinsack@gmail.com>
2021-06-02 22:22:51 +0300

    add getInitialProps to app component and pass props

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,27 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/buildClient";
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Header {currentUser.email}</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async ({ ctx, Component }) => {
+  const client = buildClient(ctx);
+  const { data } = await client.get("/api/users/currentUser");
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
