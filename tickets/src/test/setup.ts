18397da4c8e0e4e1cd6806529f4e3510479c057a
codeinsack <codeinsack@gmail.com>
2021-06-04 19:06:01 +0300

    add tickets service

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,54 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from 'supertest';
+import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signIn(): Promise<string[]>;
+    }
+  }
+}
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdf';
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  const collections = await mongoose.connection.db.collections();
+
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signIn = async () => {
+  const email = 'test@mail.io';
+  const password = 'pass123';
+
+  const response = await request(app)
+    .post('/api/users/signUp')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  return response.get('Set-Cookie');
+};
