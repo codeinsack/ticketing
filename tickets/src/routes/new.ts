ac82342fe9079026cf507a755f1d9bbc118fca8e
codeinsack <codeinsack@gmail.com>
2021-06-05 09:59:09 +0300

    fix tests for body

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,22 @@
-import express from 'express';
-import { requireAuth } from '@t1cketing/common';
+import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@t1cketing/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req, res) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  },
+);
 
 export { router as createTicketRouter };
