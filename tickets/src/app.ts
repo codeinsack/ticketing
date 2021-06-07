a4a7525976c27a733a6aaa76b5d0dc2dfc1ee5a4
codeinsack <codeinsack@gmail.com>
2021-06-07 21:49:12 +0300

    implement show ticket details route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -8,6 +8,7 @@ import {
   currentUser,
 } from '@t1cketing/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -21,6 +22,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,8 +1,10 @@
 import request from 'supertest';
+import mongoose from 'mongoose';
 import { app } from '../../app';
 
 it('Returns 404 if the ticket is not found', async () => {
-  await request(app).get('/api/tickets/sdfgasdf').send({}).expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app).get(`/api/tickets/${id}`).send({}).expect(404);
 });
 
 it('Returns the ticket if the ticket is found', async () => {
