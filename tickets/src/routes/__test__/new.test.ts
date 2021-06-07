ec3b70eb381b78a1e3da0fd723bf1f4b40003b28
codeinsack <codeinsack@gmail.com>
2021-06-07 21:12:29 +0300

    implement new ticket route

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/Ticket';
 
 it('Has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -57,6 +58,9 @@ it('Returns an error if an invalid price is provided ', async () => {
 });
 
 it('Creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
   await request(app)
     .post('/api/tickets')
     .set('Cookie', global.signIn())
@@ -65,4 +69,8 @@ it('Creates a ticket with valid inputs', async () => {
       price: 20,
     })
     .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].price).toEqual(20);
 });
