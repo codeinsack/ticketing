5d3c25addf4011ff022b18438eaa609d9cef2fa1
codeinsack <codeinsack@gmail.com>
2021-06-15 19:55:38 +0300

    add TicketCreatedPublisher instance

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { requireAuth, validateRequest } from '@t1cketing/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/Ticket';
+import { TicketCreatedPublisher } from '../events/publishers/TicketCreatedPublisher';
 
 const router = express.Router();
 
@@ -25,6 +26,12 @@ router.post(
     });
 
     await ticket.save();
+    new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   },
