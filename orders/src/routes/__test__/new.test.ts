e191becc161d15c6493cb54e5d26d6641bff811e
codeinsack <codeinsack@gmail.com>
2021-06-21 20:44:00 +0300

    write a test if is already reserved

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/Ticket';
+import { Order } from '../../models/Order';
+import { OrderStatus } from '@t1cketing/common';
 
 it('Returns an error if the ticket does not exist', async () => {
   const ticketId = mongoose.Types.ObjectId();
@@ -13,7 +16,25 @@ it('Returns an error if the ticket does not exist', async () => {
 });
 
 it('Returns an error if the ticket is already reserved', async () => {
-  //
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: 'ddfd',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signIn())
+    .send({ ticketId: ticket.id })
+    .expect(400);
 });
 
 it('Reserves a ticket', async () => {
