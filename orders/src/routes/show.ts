1ec059271d5ec49d483020d782a3a66d266e6f16
codeinsack <codeinsack@gmail.com>
2021-06-23 18:29:01 +0300

    implement get order details endpoint

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from 'express';
+import { NotAuthorizedError, NotFoundError } from '@t1cketing/common';
+import { Order } from '../models/Order';
 
 const router = express.Router();
 
 router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+  const order = await Order.findById(req.params.orderId).populate(
+    'ticket',
+  );
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  res.send(order);
 });
 
 export { router as showOrderRouter };
