51cb43f13eb5e93d2568eb3e1b90faf22ad3faa3
codeinsack <codeinsack@gmail.com>
2021-06-23 18:37:54 +0300

    add test for 401

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -24,3 +24,24 @@ it('Fetches the order', async () => {
 
   expect(fetchedOrder.id).toEqual(order.id);
 });
+
+it('Returns an error if one user tries to fetch another users order', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signIn();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signIn())
+    .send()
+    .expect(401);
+});
