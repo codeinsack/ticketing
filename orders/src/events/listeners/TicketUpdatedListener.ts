9d5f4b2c8c1e397814d6c7d4f32577b3a476b55a
codeinsack <codeinsack@gmail.com>
2021-06-27 17:41:23 +0300

    implement ticket updated listener

---
+++ orders/src/events/listeners/TicketUpdatedListener.ts
@@ -0,0 +1,24 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, Subjects, TicketUpdatedEvent } from '@t1cketing/common';
+import { queueGroupName } from './QueueGroupName';
+import { Ticket } from '../../models/Ticket';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    const { title, price } = data;
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
