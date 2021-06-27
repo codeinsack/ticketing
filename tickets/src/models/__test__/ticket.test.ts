4179027e66289b6c9dcc249954a0dacbeaae7e47
codeinsack <codeinsack@gmail.com>
2021-06-27 20:10:43 +0300

    add test for occ

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,1 +1,25 @@
-it('Implements optimistic concurrency control', async () => {});
+import { Ticket } from '../Ticket';
+
+it('Implements optimistic concurrency control', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+  await ticket.save();
+
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
+  await firstInstance!.save();
+
+  try {
+    await secondInstance!.save();
+  } catch (error) {
+    // eslint-disable-next-line no-useless-return
+    return;
+  }
+});
