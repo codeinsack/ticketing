53273a6f8b0c4a89352f30be9c4248b61e63c776
codeinsack <codeinsack@gmail.com>
2021-06-27 17:36:08 +0300

    implement ticket crated listener

--- orders/src/events/listeners/TicketCreatedListener.ts
+++ orders/src/events/listeners/TicketCreatedListener.ts
@@ -1,9 +1,21 @@
+import { Message } from 'node-nats-streaming';
 import { Listener, Subjects, TicketCreatedEvent } from '@t1cketing/common';
+import { queueGroupName } from './QueueGroupName';
+import { Ticket } from '../../models/Ticket';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
 
-  queueGroupName = 'orders-service';
+  queueGroupName = queueGroupName;
 
-  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const { title, price, id } = data;
+    const ticket = Ticket.build({
+      id,
+      title,
+      price,
+    });
+    await ticket.save();
+    msg.ack();
+  }
 }
--- orders/src/models/Ticket.ts
+++ orders/src/models/Ticket.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { Order, OrderStatus } from './Order';
 
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -38,7 +39,12 @@ const ticketSchema = new mongoose.Schema(
   },
 );
 
-ticketSchema.statics.build = (attrs: TicketAttrs) => new Ticket(attrs);
+ticketSchema.statics.build = (attrs: TicketAttrs) =>
+  new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price,
+  });
 
 ticketSchema.methods.isReserved = async function () {
   const existingOrder = await Order.findOne({
