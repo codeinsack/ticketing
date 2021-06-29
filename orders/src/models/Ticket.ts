07ac27e576c433e73e141db7d632b61a6e879c37
codeinsack <codeinsack@gmail.com>
2021-06-29 19:13:13 +0300

    add version property

--- orders/src/models/Ticket.ts
+++ orders/src/models/Ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { Order, OrderStatus } from './Order';
 
 interface TicketAttrs {
@@ -38,6 +39,10 @@ const ticketSchema = new mongoose.Schema(
     },
   },
 );
+
+ticketSchema.set('versionKey', 'version');
+
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) =>
   new Ticket({
