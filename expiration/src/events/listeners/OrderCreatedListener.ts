7c0b67145669646e21058b8475b97d5eb7fc942c
codeinsack <codeinsack@gmail.com>
2021-07-04 23:32:15 +0300

    add delay

--- expiration/src/events/listeners/OrderCreatedListener.ts
+++ expiration/src/events/listeners/OrderCreatedListener.ts
@@ -1,6 +1,7 @@
 import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@t1cketing/common';
 import { queueGroupName } from './QueueGroupName';
+import { expirationQueue } from '../../queues/ExpirationQueue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -8,6 +9,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    //
+    const delay =
+      new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      { delay },
+    );
+
+    msg.ack();
   }
 }
