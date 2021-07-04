a1c4f95afb437ccb757a7f7a97983d3dfdad190f
codeinsack <codeinsack@gmail.com>
2021-07-05 00:52:45 +0300

    add ExpirationCompleteListener

---
+++ orders/src/events/listeners/ExpirationCompleteListener.ts
@@ -0,0 +1,38 @@
+import { Message } from 'node-nats-streaming';
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  Subjects,
+} from '@t1cketing/common';
+import { queueGroupName } from './QueueGroupName';
+import { Order, OrderStatus } from '../../models/Order';
+import { OrderCancelledPublisher } from '../publishers/OrderCancelledPublisher';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+    const order = await Order.findById(data.orderId).populate('ticket');
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({
+      status: OrderStatus.Cancelled,
+    });
+
+    await order.save();
+
+    new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      version: order.version,
+      ticket: {
+        id: order.ticket.id,
+      },
+    });
+    msg.ack();
+  }
+}
