3b6eb9671bf8c39e95b60d728c640e1da1342569
codeinsack <codeinsack@gmail.com>
2021-07-05 18:26:34 +0300

    listen to ExpirationCompleteListener

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,7 @@ import { app } from './app';
 import { natsWrapper } from './natsWrapper';
 import { TicketCreatedListener } from './events/listeners/TicketCreatedListener';
 import { TicketUpdatedListener } from './events/listeners/TicketUpdatedListener';
+import { ExpirationCompleteListener } from './events/listeners/ExpirationCompleteListener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -38,6 +39,7 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
