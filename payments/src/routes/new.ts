bde866ea4b0d6fb924bb4052d9f61ef04423dd8a
codeinsack <codeinsack@gmail.com>
2021-07-06 21:48:10 +0300

    install stripe

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,14 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@t1cketing/common';
+import {
+  BadRequestError,
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from '@t1cketing/common';
 import { body } from 'express-validator';
+import { Order } from '../models/Order';
 
 const router = express.Router();
 
@@ -8,7 +16,23 @@ router.post(
   '/api/payments',
   requireAuth,
   [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
+  validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Can not pay for a cancelled order');
+    }
+
+    console.log('token', token);
     res.send({ success: true });
   },
 );
