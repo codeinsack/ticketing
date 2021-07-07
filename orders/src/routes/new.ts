bccb931f03a5d84292a7fee24b9ce8eb8efa1ff9
codeinsack <codeinsack@gmail.com>
2021-07-07 17:51:59 +0300

    change expiration time

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -15,7 +15,7 @@ import { natsWrapper } from '../natsWrapper';
 
 const router = express.Router();
 
-const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+const EXPIRATION_WINDOW_SECONDS = 30;
 
 router.post(
   '/api/orders',
