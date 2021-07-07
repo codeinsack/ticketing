35ddc3cb8534e6046c9eb4e533c8d75f76bd5ed7
codeinsack <codeinsack@gmail.com>
2021-07-07 17:13:57 +0300

    implement ticket creation UI

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,16 +1,54 @@
+import { useState } from "react";
+import useRequest from "../../hooks/useRequest";
+
 const NewTicket = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/tickets",
+    method: "post",
+    body: {
+      title,
+      price,
+    },
+    onSuccess: (ticket) => console.log("ticket", ticket),
+  });
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+    if (isNaN(value)) {
+      return;
+    }
+    setPrice(value.toFixed(2));
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    doRequest();
+  };
+
   return (
     <div>
       <h1>Create a ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
-          <input className="form-control" />
+          <input
+            className="form-control"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
         </div>
         <div className="form-group">
           <label>Price</label>
-          <input className="form-control" />
+          <input
+            className="form-control"
+            value={price}
+            onBlur={onBlur}
+            onChange={(event) => setPrice(event.target.value)}
+          />
         </div>
+        {errors}
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
